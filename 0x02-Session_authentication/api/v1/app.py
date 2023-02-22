63f327df366868138c2c7e55fa61d026443a79a3
adekunleo1 <adekunle.ogunleye1@gmail.com>
2023-02-22 14:31:48 +0100

    app.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -55,12 +55,13 @@ def authenticate_user():
             '/api/v1/forbidden/',
         ]
         if auth.require_auth(request.path, excluded_paths):
-            auth_header = auth.authorization_header(request)
             user = auth.current_user(request)
-            if auth_header is None:
+            if auth.authorization_header(request) is None and \
+                    auth.session_cookie(request) is None:
                 abort(401)
             if user is None:
                 abort(403)
+            request.current_user = user
 
 
 if __name__ == "__main__":
